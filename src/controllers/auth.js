0a7b941020cb889891e9b51cd579d8c32abc64d6
Feber Cruz <63736865+Lebers@users.noreply.github.com>
2023-12-11 22:33:10 -0600

    cliente auth ya quedó bien

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,12 +3,16 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
+const UserClient = require('../models/userClient');
+const Clientes = require('../models/clients');
+
 
 async function login(req, res, next) {
   const { email, password } = req.body;
 
   // Busca al usuario por su correo electrónico
   const user = await User.findOne({ email });
+
 
   if (!user) {
     res.status(401).json({ message: 'Credenciales inválidas' });
@@ -57,6 +61,79 @@ async function login(req, res, next) {
   });
 }
 
+async function loginclient(req, res, next) {
+  const { email, password, codigoCliente } = req.body;
+
+  // Busca al usuario por su correo electrónico
+  const user = await UserClient.findOne({ email });
+
+  console.log("user:", user);
+
+  if (!user) {
+    return res.status(401).json({ message: 'Credenciales inválidas' });
+  }
+
+  const idCliente = user.idCliente.toString();
+  console.log("idCliente:", idCliente);
+
+  const cliente = await Clientes.findOne({ _id: idCliente });
+  console.log("cliente:", cliente);
+
+  // Verifica si el cliente existe antes de acceder a sus propiedades
+  if (!cliente) {
+    console.log("El cliente no fue encontrado en la base de datos");
+    return res.status(401).json({ message: 'Credenciales inválidas' });
+  }
+
+  console.log("codigoClienteDB:", cliente.codigoCliente);
+
+  // Verifica la contraseña
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid || codigoCliente !== cliente.codigoCliente) {
+    return res.status(401).json({ message: 'Credenciales inválidas' });
+  }
+
+  console.log("Credenciales válidas");
+
+  // Genera un token JWT
+  const token = jwt.sign(
+    {
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      data: {
+        id: user._id,
+        name: user.name,
+        lastname: user.lastname,
+        position: user.position,
+        email: user.email,
+      },
+    },
+    process.env.TOKEN_SECRET,
+  );
+
+  res.status(200).json({
+    response: {
+      status: 'ok',
+      code: 200,
+      message: 'success processing',
+    },
+    payload: {
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        lastname: user.lastname,
+        position: user.position,
+        email: user.email,
+        age: user.age,
+        signature: user.signature,
+      },
+    },
+  });
+}
+
+
 module.exports = {
   login,
+  loginclient
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const authController = require('../controllers/auth');
 
 router.post('/login', authController.login);
+router.post('/login/client', authController.loginclient);
 
 module.exports = {
   authRoutes: router,
